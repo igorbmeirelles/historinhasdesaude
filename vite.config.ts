245739f32fe7a81261cba1d48eceeb1528c77037
pedromarcolino <pedromarcolino852@gmail.com>
2025-08-02 17:52:24 -0300

    feat: adicionando pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.png",
+        "apple-touch-icon.png",
+        "images/*",
+        "story/**/*",
+      ],
+      manifest: {
+        name: "Historinhas de Saúde",
+        short_name: "Historinhas",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/images/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: [
+          "**/*.{js,css,html,ico,png,svg,webmanifest,txt,mp3,ogg,jpg,jpeg}",
+        ],
+      },
+    }),
   ],
-})
+});
